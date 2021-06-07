3ed97186fa8742587c9fa1bc8afc6facec8fbb64
gibsonshelby <thomas1.paulo@epitech.eu>
2021-06-07 21:33:26 +0200

    front push

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+
 const app = {
 
-    baseUrl = "https://wildcode.herokuapp.com/users",
+    baseUrl: "https://wildcode.herokuapp.com/user",
+    getUrl: "https://wildcode.herokuapp.com/users",
 
     init:async()=> {
-        await app.getdata();
+        await app.getData();
         app.addListeners();
 
     },
@@ -19,19 +21,52 @@ const app = {
     onSubmit:async(event)=>{
         event.preventDefault();
         const formData = new FormData(event.target)
-
+        document.getElementById("name").value =""
+        console.log(event.target)
+        console.log(formData.get("name"))
+        const headers = new Headers
         try{
-            const result = await fetch(`$app.baseUrl`,{method:"POST",body:formData})
-           const jsonList =  await result.json()
-            console.log(jsonList)
+            
+            const result = await fetch(`${app.baseUrl}`,{method:"POST",body:formData,headers,mode:"cors"})
+           
+           const jsonResult =  await result.json()
+
+           app.makeDataInDom(jsonResult)
+            
 
         }catch (err){
-            console.error(err)
+         
         }
-        },
+ },
+
+    getData : async () => {
+            try{
+                const result = await fetch(`${app.getUrl}`,{method:"GET",mode:"cors"})
+              
+               const jsonList =  await result.json()
+                
+                
+                for(argonaute of jsonList){
+                    app.makeDataInDom(argonaute)
+                }
+
+            }catch (err){
+                console.error(err)
+            }
+ },
+            
 
         
-    }
+
+        makeDataInDom : data => {
+            const list = document.getElementsByClassName("member-list")
+            const el = document.createElement("div")
+            el.className = "member-item"
+            el.textContent = data.name
+            list[0].appendChild(el)
+        }
+
+}
 
 
 document.addEventListener('DOMContentLoaded',app.init)
